f3ee3d74c8864b3f4dbce255650ce2a9b140cf3b
Pablo Perez <pablo.ceti.96@hotmail.com>
2024-04-15 16:13:59 -0600

    mostrar dinamicamente el numero de productos en el carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useContext, useState } from "react";
+import { CartContext } from "../context/CartContext";
+
 const CartWidget = () => {
-    const numberOfItemsInCart = 1;
+    const { cart, setCart } = useContext(CartContext);
+    const [numberOfItemsInCart, setNumberOfItemsInCart] = useState(0);
+
+    useEffect(() => {
+      const quantityAdd = cart.reduce(function(quantity, product) {
+        return quantity + product.cantidad;
+      }, 0);
+      setNumberOfItemsInCart(quantityAdd);
+    }, [cart]);
     
     return (
       <div className="cart-widget-container text-dark fw-bold">
         <i className="bi bi-cart4 fs-1 cart-image"></i>
-        <span className="cart-item-count">{numberOfItemsInCart}</span>
+        {numberOfItemsInCart > 0 ? (
+          <span className="cart-item-count">{numberOfItemsInCart}</span>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
